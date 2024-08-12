641b3f8a1e0c3a02b42473f360a9db31b2860d8d
Vaibhav <vaibhavholani@gmail.com>
2024-08-12 14:00:12 -0400

    python 3.10 compatibility change

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@
 import sys
 import time
 from abc import abstractmethod, ABCMeta
-from collections import Iterable
+from collections.abc import Iterable
 from typing import Union, Dict, final
 
 import numpy as np
